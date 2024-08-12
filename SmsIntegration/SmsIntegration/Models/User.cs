fb0a9121b67d21ef0793ade4d206cc2acb7be5d4
Sena Ekincioğlu <ekincioglusena1@gmail.com>
2024-08-12 11:53:52 +0300

    User tablosu ( class ) oluşturuldu

---
+++ SmsIntegration/SmsIntegration/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace SendAndReceiveSmsCORE.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PhoneNumber { get; set; }
+        public string VerificationCode { get; set; } // SMS kodunu saklamak için
+        public bool IsVerified { get; set; } // Kullanıcının doğrulanıp doğrulanmadığını kontrol etmek için
+    }
+}
