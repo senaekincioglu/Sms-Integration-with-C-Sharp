e5d69a1bdf757203a4dd7ddd5bfff7ee83cb9d2a
Sena Ekincioğlu <ekincioglusena1@gmail.com>
2024-08-12 11:45:54 +0300

    Db bağlantısında class olarak DbContext den çekmesi için Data klasörü oluşturularak ApplicationDbContext oluşturuldu.

---
+++ SmsIntegration/SmsIntegration/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace SmsIntegration.Data
+
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+           : base(options)
+        {
+        }
+    }
+}
