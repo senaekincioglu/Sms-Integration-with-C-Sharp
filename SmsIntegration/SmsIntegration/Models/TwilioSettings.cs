d15d27262b1d48a911f28823755e52d7410497be
Sena Ekincioğlu <ekincioglusena1@gmail.com>
2024-08-12 11:49:39 +0300

    TwilioSettings adında bir model ( class ) oluşturuldu.

---
+++ SmsIntegration/SmsIntegration/Models/TwilioSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace SmsIntegration.Models
+{
+    public class TwilioSettings
+    {
+        public int Id { get; set; }
+        public string AccountSid { get; set; }
+        public string AuthToken { get; set; }
+        public string MyPhoneNumber { get; set; }
+        public string TwilioPhoneNumber { get; set; }
+    }
+}
