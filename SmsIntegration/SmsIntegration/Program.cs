b0e41a63c6efe0f77eb139dba000f820da6bae14
Sena Ekincioğlu <ekincioglusena1@gmail.com>
2024-08-12 11:51:59 +0300

    ApplicationDbContext sınıfı, DbContext olarak yapılandırıldı ve SQL Server veritabanı bağlantısı kurulmuştur, Ayrıca TwilioSettings adında bir konfigürasyon bölümü eklenerek bu ayarlar hizmet olarak kaydedilmiştir. Bu ayar, Twilio SMS entegrasyonu için gerekli yapılandırmaları içerir.

--- SmsIntegration/SmsIntegration/Program.cs
+++ SmsIntegration/SmsIntegration/Program.cs
@@ -1,7 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using SmsIntegration.Data;
+using SmsIntegration.Models;
+using SmsIntegration.Data;
+using SmsIntegration.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Configure DbContext with SQL Server
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Register TwilioSettings service
+builder.Services.Configure<TwilioSettings>(builder.Configuration.GetSection("Twilio"));
 
 var app = builder.Build();
 
@@ -9,7 +22,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -22,6 +34,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Sms}/{action=Register}/{id?}");
 
 app.Run();
